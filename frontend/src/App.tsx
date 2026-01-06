920ac5af9963366e4780fa610ed5b1a1027ec88f
quatrorabes <chrisrabenold@gmail.com>
2026-01-06 01:57:53 -0800

    feat: add relationship intelligence dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ import ICPsPage from './pages/ICPsPage';
 import CampaignsPage from './pages/CampaignsPage';
 import TemplatesPage from './pages/TemplatesPage';
 import { ContactDetailPage } from './pages/ContactDetailPage';
-import RelationshipIntelligence from './components/RelationshipIntelligence';
+import RelationshipIntelligence from './pages/RelationshipIntelligence';
 
 
 function App() {
